2939b6c9b0ac3730d4d3bb0e62a20651cddd1b3e
hanna <hlbernard124@gmail.com>
2017-05-30 11:45:12 -0400

    gearhost database working

---
+++ GrubTime/Migrations/20170530152528_gearhostDatabase.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GrubTime.Models;
+
+namespace GrubTime.Migrations
+{
+    [DbContext(typeof(GrubTimeContext))]
+    [Migration("20170530152528_gearhostDatabase")]
+    partial class gearhostDatabase
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("GrubTime.Models.StarredPlaces", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("IsStarred");
+
+                    b.Property<string>("PlaceId");
+
+                    b.Property<string>("UserToken");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("StarredPlaces");
+                });
+        }
+    }
+}
